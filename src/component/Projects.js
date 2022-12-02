c0a547b40d0239d81647c59ee1c1d1a02cf86c3f
Heezol06 <shahriarrahmanheezol@gmail.com>
2022-12-02 20:37:03 +0600

    second-step

--- src/component/Projects.js
+++ src/component/Projects.js
@@ -3,7 +3,92 @@ import React from 'react';
 const Projects = () => {
     return (
         <div class="hero min-h-screen text-white">
-            <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt, obcaecati.</h1>
+            <div class="hero-content flex-col lg:flex-row-reverse">
+          <div class="text-center lg:text-left">
+            <h1 class="text-5xl font-bold">ABC Company.</h1>
+            <p class="py-6">
+              ABC company planning to develop a system for the oil and gas
+              industry. This is the prototype of this company.
+            </p>
+          </div>
+          <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-red-100">
+            <div class="card-body">
+              <div class="form-control">
+                <label class="label">
+                  <span class="label-text">File Upload</span>
+                </label>
+                <input
+                  type="file"
+                  placeholder="File Upload"
+                  style={{backgroundColor:"#ae0f23"}}
+                />
+              </div>
+              <div class="form-control">
+                <label class="label">
+                  <span class="label-text">max_X</span>
+                </label>
+                <input
+                  type="number"
+                  placeholder="max_X"
+                  class="input input-bordered"
+                />
+              </div>
+              <div class="form-control">
+                <label class="label">
+                  <span class="label-text">min_X</span>
+                </label>
+                <input
+                  type="number"
+                  placeholder="min_X"
+                  class="input input-bordered"
+                />
+              </div>
+              <div class="form-control">
+                <label class="label">
+                  <span class="label-text">max_Y</span>
+                </label>
+                <input
+                  type="number"
+                  placeholder="max_Y"
+                  class="input input-bordered"
+                />
+              </div>
+              <div class="form-control">
+                <label class="label">
+                  <span class="label-text">min_y</span>
+                </label>
+                <input
+                  type="number"
+                  placeholder="min_y"
+                  class="input input-bordered"
+                />
+              </div>
+              <div class="form-control">
+                <label class="label">
+                  <span class="label-text">max_Z</span>
+                </label>
+                <input
+                  type="number"
+                  placeholder="max_Z"
+                  class="input input-bordered"
+                />
+              </div>
+              <div class="form-control">
+                <label class="label">
+                  <span class="label-text">min_Z</span>
+                </label>
+                <input
+                  type="number"
+                  placeholder="min_Z"
+                  class="input input-bordered"
+                />
+              </div>
+              <div class="form-control mt-6">
+                <button class="btn btn-error">Submit</button>
+              </div>
+            </div>
+          </div>
+        </div>
         </div>
     );
 };
